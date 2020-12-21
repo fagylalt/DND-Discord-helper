e4335e540317163d467e76445a965d9113f783ff
fagylalt <kalvinferenc@gmail.com>
2020-12-21 03:08:02 +0100

    Update bot.js
    very basic dice rolling capabilities

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
 const Discord = require('discord.js');
+const config = require('./config.json');
 const client = new Discord.Client();
 
+client.login(config.token)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('4bc13609908528bbd430f66d2b5c454463cde7873ec19a2251a70e96c2ee8f95');
+
+
+client.on('message', message =>{
+    if(message.content === "!1d20"){
+        let random = Math.floor(Math.random()*20)
+        message.channel.send("You rolled: "+random);
+    }
+});
